1eb66fb1a0a963c6d1414cbee93059ac485a7978
Gustavo Barbosa <gustavocsb@gmail.com>
2012-01-05 18:17:07 -0200

    Testing manager module.

--- asyncmongoorm/manager.py
+++ asyncmongoorm/manager.py
@@ -10,77 +10,59 @@ class Manager(object):
     def __init__(self, collection):
         self.collection = collection
     
-    @classmethod
     @gen.engine
-    def find_one(cls, query, callback):
-        logging.debug("[MongoORM] - findone %s" % query)
-
-        response, error = yield gen.Task(cls.get_collection().find_one, query=query)
-        instance = cls.create(response) if response else None
+    def find_one(self, query, callback):
+        response, error = yield gen.Task(Session(self.collection.__collection__).find_one, query=query)
+        instance = self.collection.create(response) if response else None
         
         callback(instance) 
    
-    @classmethod
     @gen.engine
-    def find(cls, query, callback, **kw):
-        logging.debug("[MongoORM] - find %s, %s" % (cls.__name__, query))
-
-        result, error = yield gen.Task(cls.get_collection().find, query=query, **kw)
+    def find(self, query, callback, **kw):
+        result, error = yield gen.Task(Session(self.collection.__collection__).find, query=query, **kw)
         items = []
         for item in result:
-            items.append(cls.create(item))
+            items.append(self.collection.create(item))
 
         callback(items)
 
-    @classmethod
     @gen.engine
-    def count(cls, query=None, callback=None, **kw):
-        db = Session()
-
+    def count(self, query=None, callback=None):
         command = {
-            "count": cls.__collection__
+            "count": self.collection.__collection__
         }
 
         if query:
             command["query"] = query
 
-        logging.debug("[MongoORM] - counting command %s" % command)
-        result, error = yield gen.Task(db.command, command)
+        result, error = yield gen.Task(Session().command, command)
         
         total = int(result['n'])
         
         callback(total)
         
-    @classmethod
     @gen.engine
-    def sum(cls, query=None, field=None, callback=None, **kw):
-        db = Session()
-
+    def sum(self, query, field, callback):
         command = {
             "group": {
-                'ns': cls.__collection__,
+                'ns': self.collection.__collection__,
                 'cond': query,
                 'initial': {'csum': 0},
                 '$reduce': 'function(obj,prev){prev.csum+=obj.'+field+';}'
             }
         }
 
-        logging.debug("[MongoORM] - sum group command %s" % command)
-        result, error = yield gen.Task(db.command, command)
+        result, error = yield gen.Task(Session().command, command)
         total = 0
         if result['retval']:
             total = result['retval'][0]['csum']
 
-        logging.debug("[MongoORM] - sum result %s" % total)
-
         callback(total)
         
-    @classmethod
     @gen.engine
-    def geo_near(cls, near, max_distance=None, num=None, spherical=None, unique_docs=None, query=None, callback=None, **kw):
-        db = Session()
+    def geo_near(self, near, max_distance=None, num=None, spherical=None, unique_docs=None, query=None, callback=None, **kw):
 
-        command = SON({"geoNear": cls.__collection__})
+        command = SON({"geoNear": self.collection.__collection__})
 
         if near != None:
             command.update({'near': near})
@@ -100,21 +82,14 @@ class Manager(object):
         if spherical != None:
             command.update({'spherical': spherical})
 
-        logging.debug("[MongoORM] - geoNear command %s" % command)
-        result, error = yield gen.Task(db.command, command)
+        result, error = yield gen.Task(Session().command, command)
         items = []
 
         if result['ok']:
             for item in result['results']:
-                items.append(cls.create(item['obj']))
+                items.append(self.collection.create(item['obj']))
         
         callback(items)
 
-    @classmethod
-    def get_collection(cls):
-        db = Session()
-        return getattr(db, cls.__collection__)
-
-    @classmethod
-    def remove_all(cls):
-        return cls.get_collection().remove()
+    def drop(self):
+        return Session(self.collection.__collection__).remove()
---
+++ tests/unit/test_manager.py
@@ -0,0 +1,152 @@
+import fudge
+import unittest2
+from tornado import gen
+from asyncmongoorm import collection
+from asyncmongoorm import manager
+from asyncmongoorm.field import StringField
+
+class ManagerTestCase(unittest2.TestCase):
+
+    @fudge.test
+    @gen.engine
+    def test_find_one(self):
+
+        class CollectionTest(collection.Collection):
+            __collection__ = 'some_collection'
+            some_attr = StringField()
+
+        def fake_find_one(query, callback):
+            self.assertEqual(query, {'_id':1})
+            callback(({'some_attr': 'some_value'}, None))
+
+        fake_session = fudge.Fake()
+        fake_session.is_callable().with_args('some_collection').returns_fake().has_attr(find_one=fake_find_one)
+
+        with fudge.patched_context(manager, 'Session', fake_session):
+            manager_object  = manager.Manager(CollectionTest)
+            instance = yield gen.Task(manager_object.find_one, {'_id': 1})
+            self.assertEquals('some_value', instance.some_attr)
+
+    @fudge.test
+    @gen.engine
+    def test_find(self):
+
+        class CollectionTest(collection.Collection):
+            __collection__ = 'some_collection'
+            some_attr = StringField()
+
+        def fake_find(query, callback, limit):
+            self.assertEqual(query, {'_id':1})
+            self.assertEquals(1, limit)
+            callback(([{'some_attr': 'some_value'}, {'some_attr': 'another_value'}], None))
+
+        fake_session = fudge.Fake()
+        fake_session.is_callable().with_args('some_collection').returns_fake().has_attr(find=fake_find)
+
+        with fudge.patched_context(manager, 'Session', fake_session):
+            manager_object  = manager.Manager(CollectionTest)
+            instances = yield gen.Task(manager_object.find, {'_id': 1}, limit=1)
+            self.assertEquals('some_value', instances[0].some_attr)
+            self.assertEquals('another_value', instances[1].some_attr)
+
+    @fudge.test
+    @gen.engine
+    def test_count_without_query(self):
+
+        fake_collection = fudge.Fake().has_attr(__collection__='some_collection')
+
+        def fake_command(command, callback):
+            self.assertEqual({"count": 'some_collection'}, command)
+            callback(({'n': 10}, None))
+
+        fake_session = fudge.Fake()
+        fake_session.is_callable().returns_fake().has_attr(command=fake_command)
+
+        with fudge.patched_context(manager, 'Session', fake_session):
+            manager_object = manager.Manager(fake_collection)
+            result = yield gen.Task(manager_object.count)
+            self.assertEquals(10, result)
+
+    @fudge.test
+    @gen.engine
+    def test_count_with_query(self):
+
+        fake_collection = fudge.Fake().has_attr(__collection__='some_collection')
+
+        def fake_command(command, callback):
+            self.assertEqual('some_collection', command['count'])
+            self.assertEqual({'tag': 'some_tag'}, command['query'])
+            callback(({'n': 10}, None))
+
+        fake_session = fudge.Fake()
+        fake_session.is_callable().returns_fake().has_attr(command=fake_command)
+
+        with fudge.patched_context(manager, 'Session', fake_session):
+            manager_object = manager.Manager(fake_collection)
+            result = yield gen.Task(manager_object.count, query={'tag': 'some_tag'})
+            self.assertEquals(10, result)
+
+    @fudge.test
+    @gen.engine
+    def test_sum(self):
+
+        fake_collection = fudge.Fake().has_attr(__collection__='some_collection')
+
+        def fake_command(command, callback):
+            self.assertEqual('some_collection', command['group']['ns'])
+            self.assertEqual({'tag': 'some_tag'}, command['group']['cond'])
+            self.assertEqual({'csum': 0}, command['group']['initial'])
+            self.assertEqual('function(obj,prev){prev.csum+=obj.some_field;}', command['group']['$reduce'])
+            callback(({'retval': [{'csum':20}]}, None))
+
+        fake_session = fudge.Fake()
+        fake_session.is_callable().returns_fake().has_attr(command=fake_command)
+
+        with fudge.patched_context(manager, 'Session', fake_session):
+            manager_object = manager.Manager(fake_collection)
+            result = yield gen.Task(manager_object.sum, {'tag': 'some_tag'}, 'some_field')
+            self.assertEqual(20, result)
+
+    @fudge.test
+    @gen.engine
+    def test_geo_near(self):
+
+        fake_collection = fudge.Fake().has_attr(__collection__='some_collection')\
+                                .expects('create').with_args({'key': 'value'}).returns('should_be_instance')
+
+        def fake_command(command, callback):
+            expected_command = {
+                'geoNear': 'some_collection',
+                'near': 'near_value',
+                'query': {'tag': 'some_tag'},
+                'num': 10,
+                'maxDistance': 100,
+                'uniqueDocs': True,
+                'spherical': False
+            }
+            self.assertEqual(expected_command, command)
+            callback(({'ok': 1, 'results':[{'obj':{'key':'value'}}]}, None))
+
+        fake_session = fudge.Fake()
+        fake_session.is_callable().returns_fake().has_attr(command=fake_command)
+
+        with fudge.patched_context(manager, 'Session', fake_session):
+            manager_object = manager.Manager(fake_collection)
+            result = yield gen.Task(manager_object.geo_near,
+                                        'near_value',
+                                        max_distance=100,
+                                        unique_docs=True,
+                                        spherical=False,
+                                        num=10,
+                                        query={'tag': 'some_tag'})
+            self.assertEquals(['should_be_instance'], result)
+
+    @fudge.test
+    def test_drop(self):
+        fake_session = fudge.Fake()
+        fake_session.is_callable().with_args('some_collection').returns_fake().expects('remove')
+
+        fake_collection = fudge.Fake().has_attr(__collection__='some_collection')
+        with fudge.patched_context(manager, 'Session', fake_session):
+            manager_object = manager.Manager(fake_collection)
+            manager_object.drop()
